dac0a17d9b2f09dda5a02e22617ddf7c89466362
stufkan <stufkan@gmail.com>
2014-06-29 16:16:11 +0200

    Inital Modeling as in draft 1

---
+++ IrRelevant/Model/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrRelevant.Model
+{
+    public enum IconPlaceholder { Book, Video, List};
+    public class Item
+    {
+        public string Title { get; set; }
+        public IconPlaceholder Icon { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ IrRelevant/Model/Mode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrRelevant.Model
+{
+    public class Mode
+    {
+        public string Title { get; set; }
+
+        private List<SubCategory> subCategories;
+
+        public List<SubCategory> SubCategories
+        {
+            get { return subCategories; }
+        }
+
+        public List<Item> items;
+
+        public List<Item> Items { get { return items; } }
+        
+    }
+}
