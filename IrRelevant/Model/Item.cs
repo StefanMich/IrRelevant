e8668d24b85968df6f40a999f09ec841f208b26c
stufkan <stufkan@gmail.com>
2014-07-20 00:13:40 +0200

    Default constructor with default values

--- IrRelevant/Model/Item.cs
+++ IrRelevant/Model/Item.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IrRelevant;
 
 namespace IrRelevant.Model
 {
-    public enum IconPlaceholder { Book, Video, List};
     public class Item
     {
+        public const string defaultTitle = "New note";
+        public const string defaultContent = "Write your note";
+        public const IconEnum defaultIcon = IconEnum.unknown;
+
+        public Item()
+        {
+            Title = defaultTitle;
+            Icon = defaultIcon;
+            Content = defaultContent;
+        }
+
         public string Title { get; set; }
-        public IconPlaceholder Icon { get; set; }
+        public IconEnum Icon { get; set; }
         public string Content { get; set; }
     }
 }
